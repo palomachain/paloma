3cc5deb19fa96f3ec862ba6a164d3dc3ca95e6e6
Matija Martinić <matija.martinic@gmail.com>
2022-03-25 17:00:52 +0100

    Testing in CI (#20)

--- x/scheduler/keeper/setup_test.go
+++ x/scheduler/keeper/setup_test.go
@@ -1,9 +1,13 @@
 package keeper_test
 
 import (
+	"context"
+
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	tmproto "github.com/tendermint/tendermint/proto/tendermint/types"
 	"github.com/volumefi/cronchain/app"
+	"github.com/volumefi/cronchain/x/scheduler/keeper"
+	"github.com/volumefi/cronchain/x/scheduler/types"
 )
 
 func appForTesting() (app.TestApp, sdk.Context) {
@@ -11,4 +15,8 @@ func appForTesting() (app.TestApp, sdk.Context) {
 	ctx := app.BaseApp.NewContext(false, tmproto.Header{})
 
 	return app, ctx
+}
+func setupMsgServer() (types.MsgServer, app.TestApp, context.Context) {
+	testApp, ctx := appForTesting()
+	return keeper.NewMsgServerImpl(testApp.SchedulerKeeper), testApp, sdk.WrapSDKContext(ctx)
 }
