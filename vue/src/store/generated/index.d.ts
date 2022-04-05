2a5ad0dbb4ba52bc018af65f4ed70b27259675ff
Matija Martinic <matija.martinic@gmail.com>
2022-04-05 11:35:55 +0200

    Removing vue

--- vue/src/store/generated/index.d.ts
+++
@@ -1,4 +0,0 @@
-declare const _default: {
-    VolumefiCronchainVolumefiCronchainScheduler: (store: any) => void;
-};
-export default _default;
--- vue/src/store/generated/volumefi/cronchain/volumefi.cronchain.scheduler/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { SchedulerPacketData } from "./module/types/scheduler/packet";
-import { NoData } from "./module/types/scheduler/packet";
-import { Params } from "./module/types/scheduler/params";
-export { SchedulerPacketData, NoData, Params };
-declare const _default;
-export default _default;
