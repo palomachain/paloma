cdbd4b8146b73be5260289ace00350585e7794ed
Matija Martinić <matija.martinic@gmail.com>
2022-04-26 18:19:48 +0200

    query validator info (#89)

---
+++ x/valset/keeper/grpc_query_validator_info.go
@@ -0,0 +1,33 @@
+package keeper
+
+import (
+	"context"
+
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/volumefi/cronchain/x/valset/types"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/status"
+)
+
+// ValidatorInfo returns validator info. It's not related to a snapshot.
+func (k Keeper) ValidatorInfo(goCtx context.Context, req *types.QueryValidatorInfoRequest) (*types.QueryValidatorInfoResponse, error) {
+	if req == nil {
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+	valAddr, err := sdk.ValAddressFromBech32(req.ValAddr)
+
+	if err != nil {
+		return nil, err
+	}
+
+	val, err := k.getValidator(ctx, valAddr)
+	if err != nil {
+		return nil, err
+	}
+
+	return &types.QueryValidatorInfoResponse{
+		Validator: val,
+	}, nil
+}
