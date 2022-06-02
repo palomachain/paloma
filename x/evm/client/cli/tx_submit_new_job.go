bb1d780dac10cc6c64a2d3d5b9e43e928b79b6c3
Matija Martinić <matija.martinic@gmail.com>
2022-06-02 09:52:16 +0200

    Added ABI support for the EVM message job (#183)

--- x/evm/client/cli/tx_submit_new_job.go
+++ x/evm/client/cli/tx_submit_new_job.go
@@ -17,9 +17,9 @@ type submitNewJobPayloadJson struct {
 
 func CmdSubmitNewJob() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:   "submit-new-job [smart contract address] [smart contract payload]",
+		Use:   "submit-new-job [smart contract address] [smart contract payload] [smart contract JSON abi]",
 		Short: "Broadcast message SubmitNewJob",
-		Args:  cobra.ExactArgs(2),
+		Args:  cobra.ExactArgs(3),
 		RunE: func(cmd *cobra.Command, args []string) (err error) {
 
 			clientCtx, err := client.GetClientTxContext(cmd)
@@ -31,6 +31,7 @@ func CmdSubmitNewJob() *cobra.Command {
 				Creator:                 clientCtx.GetFromAddress().String(),
 				HexSmartContractAddress: args[0],
 				HexPayload:              args[1],
+				Abi:                     args[2],
 			}
 			if err := msg.ValidateBasic(); err != nil {
 				return err
