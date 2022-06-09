f7bc08bf967fee1b588dd84afb5dbb3d4631b2ca
Matija Martinić <matija.martinic@gmail.com>
2022-06-09 15:15:16 +0200

    Add pprof for paloma #213

--- cmd/palomad/main.go
+++ cmd/palomad/main.go
@@ -1,10 +1,15 @@
 package main
 
 import (
+	"fmt"
 	"os"
+
+	"net/http"
+	"net/http/pprof"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
 	"github.com/palomachain/paloma/app"
+	"github.com/spf13/cobra"
 	"github.com/tendermint/starport/starport/pkg/cosmoscmd"
 )
 
@@ -16,6 +21,32 @@ func main() {
 		app.Name,
 		app.ModuleBasics,
 		app.New,
+		cosmoscmd.CustomizeStartCmd(func(cmd *cobra.Command) {
+			oldPreRunE := cmd.PreRunE
+			cmd.PreRunE = func(cmd *cobra.Command, args []string) error {
+				go func() {
+					listen := os.Getenv("PPROF_LISTEN")
+					if len(listen) > 0 {
+						fmt.Println("enabling PPROF")
+						mux := http.NewServeMux()
+
+						mux.HandleFunc("/debug/pprof/", pprof.Index)
+						mux.HandleFunc("/debug/pprof/cmdline", pprof.Cmdline)
+						mux.HandleFunc("/debug/pprof/profile", pprof.Profile)
+						mux.HandleFunc("/debug/pprof/symbol", pprof.Symbol)
+						mux.HandleFunc("/debug/pprof/trace", pprof.Trace)
+
+						if err := http.ListenAndServe(listen, mux); err != nil {
+							panic(err)
+						}
+					}
+				}()
+				if oldPreRunE != nil {
+					return oldPreRunE(cmd, args)
+				}
+				return nil
+			}
+		}),
 		// this line is used by starport scaffolding # root/arguments
 	)
 	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
