2f44295425cff2ddf7a4b757cce54a110f8d993d
Matija Martinić <matija.martinic@gmail.com>
2022-06-20 19:13:48 +0200

    Make ugrain default denom #250

---
+++ app/consts.go
@@ -0,0 +1,10 @@
+package app
+
+const (
+
+	// BondDenom defines the native staking token denomination.
+	BondDenom = "ugrain"
+
+	// DisplayDenom defines the name, symbol, and display value of the umee token.
+	DisplayDenom = "GRAIN"
+)
