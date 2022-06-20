853dbce333294c6968690e224b7a4ecdd90d48a6
Chase Albert <chase@volume.finance>
2022-06-20 14:39:53 -0400

    Update testnet configs for paloma-testnet-4

--- scripts/setup-volume-testnet.sh
+++ scripts/setup-volume-testnet.sh
@@ -12,7 +12,9 @@ if [[ -z "${CHAIN_ID:-}" ]]; then
   exit 1
 fi
 
-VALIDATOR_STAKE_AMOUNT=100000000grain
+VALIDATOR_STAKE_AMOUNT=100000000000000ugrain
+GENESIS_AMOUNT=2500000000000000ugrain
+FAUCET_AMOUNT=500000000000000ugrain
 
 jq-i() {
   edit="$1"
@@ -35,18 +37,18 @@ name="chase"
 echo "$MNEMONIC" | palomad keys add "$name" --recover
 address="$(palomad keys show "$name" -a)"
 
-palomad add-genesis-account "$address" 250000000grain
+palomad add-genesis-account "$address" "$GENESIS_AMOUNT"
 palomad gentx "$name" "$VALIDATOR_STAKE_AMOUNT" --chain-id "$CHAIN_ID"
 
 init() {
   name="$1"
   address="$2"
-  amount="${3:-250000000grain}"
+  amount="${3:-"$GENESIS_AMOUNT"}"
 
   palomad add-genesis-account "$address" "$amount"
 }
 
-init faucet paloma167rf0jmkkkqp9d4awa8rxw908muavqgghtw6tn 500000000grain
+init faucet paloma167rf0jmkkkqp9d4awa8rxw908muavqgghtw6tn "$FAUCET_AMOUNT"
 init jason paloma1mre80u0mmsdpf3l2shre9g4sh7kp9lxu5gtlql
 init taariq paloma1k4hfe8cqdzy6j0t7sppujhrplhp8k5tglf8v46
 init vera paloma1nvhryyg63e8vw9t5sx9ht8l77jzgjvrq6jcdnp
