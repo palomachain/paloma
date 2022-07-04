fd11df9e850a916f16cc40b8b728365c8615a057
Matija Martinic <matija.martinic@gmail.com>
2022-07-04 10:23:32 +0200

    stuff

--- x/evm/keeper/grpc_query_chains_infos.go
+++ x/evm/keeper/grpc_query_chains_infos.go
@@ -3,21 +3,25 @@ package keeper
 import (
 	"context"
 
-    "github.com/palomachain/paloma/x/evm/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/palomachain/paloma/x/evm/types"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
 
-func (k Keeper) ChainsInfos(goCtx context.Context,  req *types.QueryChainsInfosRequest) (*types.QueryChainsInfosResponse, error) {
+func (k Keeper) ChainsInfos(goCtx context.Context, req *types.QueryChainsInfosRequest) (*types.QueryChainsInfosResponse, error) {
 	if req == nil {
-        return nil, status.Error(codes.InvalidArgument, "invalid request")
-    }
+		return nil, status.Error(codes.InvalidArgument, "invalid request")
+	}
 
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-    // TODO: Process the query
-    _ = ctx
+	chainsInfos, err := k.getAllChainInfos(ctx)
+	if err != nil {
+		return nil, err
+	}
 
-	return &types.QueryChainsInfosResponse{}, nil
+	return &types.QueryChainsInfosResponse{
+		ChainsInfos: chainsInfos,
+	}, nil
 }
