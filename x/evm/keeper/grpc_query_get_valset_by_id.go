85c79953486350d94c4c4d8219dfa81b4d7bd9c6
webelf101 <m63974529@gmail.com>
2022-12-16 22:04:34 +0800

    update log with sdk.context

--- x/evm/keeper/grpc_query_get_valset_by_id.go
+++ x/evm/keeper/grpc_query_get_valset_by_id.go
@@ -6,7 +6,6 @@ import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/palomachain/paloma/x/evm/types"
 	valsettypes "github.com/palomachain/paloma/x/valset/types"
-	"github.com/tendermint/tendermint/libs/log"
 	"google.golang.org/grpc/codes"
 	"google.golang.org/grpc/status"
 )
@@ -17,13 +16,14 @@ func (k Keeper) GetValsetByID(goCtx context.Context, req *types.QueryGetValsetBy
 	if req == nil {
 		return nil, status.Error(codes.InvalidArgument, "invalid request")
 	}
-	logger := log.NewNopLogger().With("module", "x/evm/keeper/grpc_query_get_valset_by_id")
+
+	ctx := sdk.UnwrapSDKContext(goCtx)
+
+	logger := k.Logger(ctx)
 	logger.Info("request info",
 		"chain-reference-id", req.GetChainReferenceID(),
 		"valset-id", req.GetValsetID(),
 	)
-
-	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	var snapshot *valsettypes.Snapshot
 	var err error
@@ -37,7 +37,7 @@ func (k Keeper) GetValsetByID(goCtx context.Context, req *types.QueryGetValsetBy
 	if err != nil {
 		return nil, err
 	}
-	valset := transformSnapshotToCompass(snapshot, req.GetChainReferenceID())
+	valset := transformSnapshotToCompass(snapshot, req.GetChainReferenceID(), logger)
 	logger.Info("request info",
 		"chain-reference-id", req.GetChainReferenceID(),
 		"valset-id", req.GetValsetID(),
--- x/evm/keeper/keeper.go
+++ x/evm/keeper/keeper.go
@@ -186,7 +186,8 @@ func (k Keeper) deploySmartContractToChain(ctx sdk.Context, chainInfo *types.Cha
 	default:
 		return err
 	}
-	valset := transformSnapshotToCompass(snapshot, chainInfo.GetChainReferenceID())
+	logger := k.Logger(ctx)
+	valset := transformSnapshotToCompass(snapshot, chainInfo.GetChainReferenceID(), logger)
 
 	if !isEnoughToReachConsensus(valset) {
 		k.Logger(ctx).Info(
@@ -621,8 +622,9 @@ func (k Keeper) OnSnapshotBuilt(ctx sdk.Context, snapshot *valsettypes.Snapshot)
 	if err != nil {
 		panic(err)
 	}
+	logger := k.Logger(ctx)
 	for _, chain := range chainInfos {
-		valset := transformSnapshotToCompass(snapshot, chain.GetChainReferenceID())
+		valset := transformSnapshotToCompass(snapshot, chain.GetChainReferenceID(), logger)
 		if !chain.IsActive() {
 			k.Logger(ctx).Info("ignoring valset for chain as the chain is not yet active",
 				"chain-reference-id", chain.GetChainReferenceID(),
@@ -733,15 +735,13 @@ func isEnoughToReachConsensus(val types.Valset) bool {
 	return sum >= thresholdForConsensus
 }
 
-func transformSnapshotToCompass(snapshot *valsettypes.Snapshot, chainReferenceID string) types.Valset {
-	logger := log.NewNopLogger().With("module", "x/evm/keeper/keeper")
+func transformSnapshotToCompass(snapshot *valsettypes.Snapshot, chainReferenceID string, logger log.Logger) types.Valset {
 	logger.Info("transformSnapshotToCompass",
 		"snapshot-id", snapshot.Id,
 		"snapshot-height", snapshot.Height,
 		"snapshot-total-shares", snapshot.TotalShares,
 		"snapshot-validators-length", len(snapshot.Validators),
 	)
-
 	validators := make([]valsettypes.Validator, len(snapshot.GetValidators()))
 	copy(validators, snapshot.GetValidators())
 
