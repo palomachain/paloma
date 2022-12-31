85ee6789c4ba9ce4c7d2d008d70c61adaf1d34dd
taariq <taariq.lewis@gmail.com>
2022-12-30 21:44:50 -0800

    add updates #611

--- x/consensus/keeper/attest.go
+++ x/consensus/keeper/attest.go
@@ -20,7 +20,12 @@ func (k Keeper) CheckAndProcessAttestedMessages(ctx sdk.Context) error {
 			}
 
 			for _, msg := range msgs {
-				k.Logger(ctx).Info("check-and-process-attested-messages-queue", "id", msg.GetId(), "nonce", msg.Nonce(), "string", msg.String())
+				k.Logger(ctx).Info
+				("check-and-process-attested-messages-queue",
+				"id", msg.GetId(),
+				"nonce", msg.Nonce(),
+				"string", fmt.Sprintf("+%v", msg),
+			)
 				cq, err := k.getConsensusQueue(ctx, opt.QueueTypeName)
 				if err != nil {
 					return err
