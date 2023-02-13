a79fb64a09dbe06bd97b71605e3b3e3b32328f09
webelf101 <m63974529@gmail.com>
2023-02-14 05:58:37 +0800

    update logs

--- x/scheduler/keeper/msg_server_create_job.go
+++ x/scheduler/keeper/msg_server_create_job.go
@@ -2,6 +2,7 @@ package keeper
 
 import (
 	"context"
+	"fmt"
 
 	sdk "github.com/cosmos/cosmos-sdk/types"
 	"github.com/palomachain/paloma/x/scheduler/types"
@@ -11,7 +12,7 @@ func (k msgServer) CreateJob(goCtx context.Context, msg *types.MsgCreateJob) (*t
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
 	var err error
-
+	fmt.Printf("[msgcreatejob_msg] UNPACK ARGS: %+v\n", msg)
 	job := msg.Job
 	job.Owner, err = sdk.AccAddressFromBech32(msg.GetCreator())
 	if err != nil {
