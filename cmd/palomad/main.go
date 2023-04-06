de5f9ee74233ebf6ba477f9b4259ad0c236a4e0d
Aleksandr Bezobchuk <aleks.bezobchuk@gmail.com>
2023-04-06 16:55:38 -0400

    updates

--- cmd/palomad/main.go
+++ cmd/palomad/main.go
@@ -4,12 +4,13 @@ import (
 	"os"
 
 	svrcmd "github.com/cosmos/cosmos-sdk/server/cmd"
+
 	"github.com/palomachain/paloma/app"
 )
 
 func main() {
 	rootCmd := NewRootCmd()
-	if err := svrcmd.Execute(rootCmd, app.DefaultNodeHome); err != nil {
+	if err := svrcmd.Execute(rootCmd, "", app.DefaultNodeHome); err != nil {
 		os.Exit(1)
 	}
 }
