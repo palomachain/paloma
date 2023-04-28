f53a0986c79354a1ab3825f703ee6bd158171342
Tyler Ruppert <{ID}+{username}@users.noreply.github.com>
2023-04-28 15:03:33 -0600

    Upgrade the way of handling messages
    It was pointed out to us that the old way of handling messages (which
    we were doing with the handler.go) is no longer supported in
    cosmos-sdk v0.47.1  We have to update.  This does that

--- x/paloma/module.go
+++ x/paloma/module.go
@@ -124,6 +124,7 @@ func (AppModule) QuerierRoute() string { return types.QuerierRoute }
 // RegisterServices registers a GRPC query service to respond to the
 // module-specific GRPC queries.
 func (am AppModule) RegisterServices(cfg module.Configurator) {
+	types.RegisterMsgServer(cfg.MsgServer(), keeper.NewMsgServerImpl(am.keeper))
 	types.RegisterQueryServer(cfg.QueryServer(), am.keeper)
 }
 
