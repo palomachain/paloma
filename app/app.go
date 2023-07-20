330e0032c1475083cf01105e1b36de2fca796800
Tyler Ruppert <{ID}+{username}@users.noreply.github.com>
2023-07-20 13:24:57 -0600

    Increase minimum pigeon version
    This release of Paloma requires features from the paired pigeon release
    in order to function properly.  Pigeons that are not at this version
    will be jailed.

--- app/app.go
+++ app/app.go
@@ -141,7 +141,7 @@ const (
 
 	wasmAvailableCapabilities = "iterator,staking,stargate,paloma"
 
-	minimumPigeonVersion = "v1.4.0"
+	minimumPigeonVersion = "v1.5.0"
 )
 
 func getGovProposalHandlers() []govclient.ProposalHandler {
