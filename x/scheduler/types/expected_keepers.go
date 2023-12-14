307adc5c753f39f45a12f02fd2408457f9a2b88b
deepan95dev <deepan@vitwit.com>
2023-12-14 13:46:57 +0530

    feat: upgraded the app with scheduler module to SDK v0.50

--- x/scheduler/types/expected_keepers.go
+++ x/scheduler/types/expected_keepers.go
@@ -1,33 +1,41 @@
 package types
 
 import (
+	"context"
 	sdk "github.com/cosmos/cosmos-sdk/types"
-	"github.com/cosmos/cosmos-sdk/x/auth/types"
 	xchain "github.com/palomachain/paloma/internal/x-chain"
 	valsettypes "github.com/palomachain/paloma/x/valset/types"
 )
 
 // AccountKeeper defines the expected account keeper used for simulations (noalias)
+//
+//go:generate mockery --name=AccountKeeper
 type AccountKeeper interface {
-	GetAccount(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
-	NewAccountWithAddress(ctx sdk.Context, addr sdk.AccAddress) types.AccountI
-	HasAccount(ctx sdk.Context, addr sdk.AccAddress) bool
-	SetAccount(ctx sdk.Context, acc types.AccountI)
+	GetAccount(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+	NewAccountWithAddress(ctx context.Context, addr sdk.AccAddress) sdk.AccountI
+	HasAccount(ctx context.Context, addr sdk.AccAddress) bool
+	SetAccount(ctx context.Context, acc sdk.AccountI)
 }
 
 // BankKeeper defines the expected interface needed to retrieve account balances.
+//
+//go:generate mockery --name=BankKeeper
 type BankKeeper interface {
-	SpendableCoins(ctx sdk.Context, addr sdk.AccAddress) sdk.Coins
+	SpendableCoins(ctx context.Context, addr sdk.AccAddress) sdk.Coins
 	// Methods imported from bank should be defined here
 }
 
 // EvmKeeper defines the expected interface for interacting with the evm module
+//
+//go:generate mockery --name=EvmKeeper
 type EvmKeeper interface {
-	PreJobExecution(ctx sdk.Context, job *Job) error
-	PickValidatorForMessage(ctx sdk.Context, chainReferenceID string, req *xchain.JobRequirements) (string, error)
+	PreJobExecution(ctx context.Context, job *Job) error
+	PickValidatorForMessage(ctx context.Context, chainReferenceID string, req *xchain.JobRequirements) (string, error)
 }
 
 // ValsetKeeper defines the expected interface for interacting with the valset module
+//
+//go:generate mockery --name=ValsetKeeper
 type ValsetKeeper interface {
-	GetCurrentSnapshot(ctx sdk.Context) (*valsettypes.Snapshot, error)
+	GetCurrentSnapshot(ctx context.Context) (*valsettypes.Snapshot, error)
 }
