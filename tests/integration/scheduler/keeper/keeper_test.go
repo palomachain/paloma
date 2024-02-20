f3f10e8f50d50a0f2f833cfcd919dd5cdb1ecec7
vishal <kannavish329@gmail.com>
2024-02-20 12:39:08 +0530

    fix: fixed lint issue

--- tests/integration/scheduler/keeper/keeper_test.go
+++ tests/integration/scheduler/keeper/keeper_test.go
@@ -54,15 +54,6 @@ const (
 	minimumPigeonVersion = "v1.10.0"
 )
 
-var maccPerms = map[string][]string{
-	authtypes.FeeCollectorName:     nil,
-	distrtypes.ModuleName:          nil,
-	minttypes.ModuleName:           {authtypes.Minter},
-	stakingtypes.BondedPoolName:    {authtypes.Burner, authtypes.Staking},
-	stakingtypes.NotBondedPoolName: {authtypes.Burner, authtypes.Staking},
-	govtypes.ModuleName:            {authtypes.Burner},
-}
-
 type fixture struct {
 	ctx sdk.Context
 
