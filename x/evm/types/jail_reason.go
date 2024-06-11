0abb4ee03c30d0e86e0127230d22b24f1d5eb948
Luis Carvalho <luis@volume.finance>
2024-06-11 14:02:52 +0100

    chore: jail validators with invalid balances
    If validators have invalid (or missing) balances, we jail them.
    Alternatively we could leave them out of the relayer pool, but this
    makes it more visible when something has failed.

--- x/evm/types/jail_reason.go
+++ x/evm/types/jail_reason.go
@@ -2,4 +2,5 @@ package types
 
 const (
 	JailReasonNotEnoughFunds = "not enough funds in the target chain (%s). Has %s. Min required: %s."
+	JailReasonInvalidBalance = "invalid balance information in the target chain (%s): %s."
 )
