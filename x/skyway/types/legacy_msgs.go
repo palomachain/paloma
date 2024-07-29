f2367338e1cadf4f5121c791f86324153080b347
Luis Carvalho <luis@volume.finance>
2024-07-29 16:28:30 +0100

    fix: add legacy skyway message (#1234)
    # Related Github tickets
    
    - Closes https://github.com/VolumeFi/paloma/issues/1921
    
    # Background
    
    We need these messages to be registered so they can be correctly
    unmarshalled, both on genesis import/export, as well as pruning the
    skyway attestation queue.
    
    # Testing completed
    
    - [x] test coverage exists or has been added/updated
    - [x] tested in a private testnet
    
    # Breaking changes
    
    - [x] I have checked my code for breaking changes
    - [x] If there are breaking changes, there is a supporting migration.

---
+++ x/skyway/types/legacy_msgs.go
@@ -0,0 +1,72 @@
+package types
+
+import (
+	fmt "fmt"
+
+	sdkerrors "cosmossdk.io/errors"
+	"github.com/cometbft/cometbft/crypto/tmhash"
+	sdk "github.com/cosmos/cosmos-sdk/types"
+	"github.com/palomachain/paloma/util/libmeta"
+)
+
+var _ sdk.Msg = &MsgBatchSendToEthClaim{}
+
+var _ EthereumClaim = &MsgBatchSendToEthClaim{}
+
+func (msg *MsgBatchSendToEthClaim) SetOrchestrator(orchestrator sdk.AccAddress) {
+	msg.Orchestrator = orchestrator.String()
+}
+
+// GetType returns the claim type
+func (msg *MsgBatchSendToEthClaim) GetType() ClaimType {
+	return CLAIM_TYPE_BATCH_SEND_TO_ETH
+}
+
+// ValidateBasic performs stateless checks
+func (e *MsgBatchSendToEthClaim) ValidateBasic() error {
+	if err := libmeta.ValidateBasic(e); err != nil {
+		return err
+	}
+	if e.EventNonce == 0 {
+		return fmt.Errorf("event_nonce == 0")
+	}
+	if e.SkywayNonce == 0 {
+		return fmt.Errorf("skyway_nonce == 0")
+	}
+	if e.BatchNonce == 0 {
+		return fmt.Errorf("batch_nonce == 0")
+	}
+	if err := ValidateEthAddress(e.TokenContract); err != nil {
+		return sdkerrors.Wrap(err, "erc20 token")
+	}
+	return nil
+}
+
+// Hash implements WithdrawBatch.Hash
+func (msg *MsgBatchSendToEthClaim) ClaimHash() ([]byte, error) {
+	path := fmt.Sprintf("%d/%d/%d/%s", msg.SkywayNonce, msg.EthBlockHeight, msg.BatchNonce, msg.TokenContract)
+	return tmhash.Sum([]byte(path)), nil
+}
+
+func (msg MsgBatchSendToEthClaim) GetClaimer() sdk.AccAddress {
+	err := msg.ValidateBasic()
+	if err != nil {
+		panic(fmt.Errorf("MsgBatchSendToEthClaim failed ValidateBasic! Should have been handled earlier: %v", err))
+	}
+	val, err := sdk.AccAddressFromBech32(msg.Orchestrator)
+	if err != nil {
+		panic(fmt.Errorf("Invalid orchestrator: %v", err))
+	}
+	return val
+}
+
+// GetSigners defines whose signature is required
+func (msg MsgBatchSendToEthClaim) GetSigners() []sdk.AccAddress {
+	return libmeta.GetSigners(&msg)
+}
+
+// Route should return the name of the module
+func (msg MsgBatchSendToEthClaim) Route() string { return RouterKey }
+
+// Type should return the action
+func (msg MsgBatchSendToEthClaim) Type() string { return "batch_send_to_eth_claim" }
